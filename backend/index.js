3f196774729b61d62424a8d38835fcb73d2d79df
jhony1226 <38925931+jhony1226@users.noreply.github.com>
2022-02-21 22:13:38 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ import express from "express";
 import cors from "cors";
 import db from "./db/db.js";
 import dotenv from "dotenv"; 
-import scrumRole from "./routes/scrumRole.js"; 
+ 
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/scrumRole", scrumRole); 
+ 
 
 app.listen(process.env.PORT, () =>
   console.log("Backend server running on port: " + process.env.PORT)
